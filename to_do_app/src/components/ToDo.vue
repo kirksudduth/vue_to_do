0fdbd889248f4bc6cf1371d14c386f6031369223
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-11 14:00:28 -0600

    used the cli install instead and it is working

---
+++ to_do_app/src/components/ToDo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="to_do">
+        <h1>{{ msg }}</h1>
+        <input type="text"/>
+        <h2>Your To Do's</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ToDo',
+    data () {
+        return {
+            msg: 'This is VueToDo'
+        }
+    }
+}
+</script>
