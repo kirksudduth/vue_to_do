c1f06d110b84d2e79022b0bd2bbe4a4ae7c7379a
Kirk Sudduth <kirksudduth@gmail.com>
2020-11-14 15:35:36 -0600

    edited index.html and added form tag to my todo component

--- to_do_app/src/components/ToDo.vue
+++ to_do_app/src/components/ToDo.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="to_do">
         <h1>{{ msg }}</h1>
+        <form method="POST">
         <input type="text" placeholder="Whatchyah wanna do"/>
         <input type="submit" value="New Todo">
+        </form>
         <h2>Your To Do's</h2>
     </div>
 </template> 
